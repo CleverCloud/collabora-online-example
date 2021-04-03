77e12bac5aa48ddcd18a8a5f2a164f00380d792d
Gaël Reyrol <me@gaelreyrol.dev>
2021-04-03 15:36:52 +0200

    feat: add domain for WOPI

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+# Collabora Online Server
+
+> This custom setup purpose is to be deployed on Clever Cloud
+
+It is inspired from original Collabora Online Docker [setup](https://github.com/CollaboraOnline/online/tree/master/docker).
--- scripts/start-collabora-online.sh
+++ scripts/start-collabora-online.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env sh
 
 # Replace trusted host and set admin username and password
+perl -pi -e "s/localhost<\/host>/${DOMAIN}<\/host>/g" /etc/loolwsd/loolwsd.xml
 perl -pi -e "s/<username (.*)>.*<\/username>/<username \1>${USERNAME}<\/username>/" /etc/loolwsd/loolwsd.xml
 perl -pi -e "s/<password (.*)>.*<\/password>/<password \1>${PASSWORD}<\/password>/" /etc/loolwsd/loolwsd.xml
 perl -pi -e "s/<server_name (.*)>.*<\/server_name>/<server_name \1>${SERVER_NAME}<\/server_name>/" /etc/loolwsd/loolwsd.xml
-perl -pi -e "s/<allowed_languages (.*)>.*<\/allowed_languages>/<allowed_languages \1>${DICTIONARIES:-de_DE en_GB en_US es_ES fr_FR it nl pt_BR pt_PT ru}<\/allowed_languages>/" /etc/loolwsd/loolwsd.xml
+perl -pi -e "s/<allowed_languages (.*)>.*<\/allowed_languages>/<allowed_languages \1>${DICTIONARIES:-en_GB en_US fr_FR}<\/allowed_languages>/" /etc/loolwsd/loolwsd.xml
 
 # Generate WOPI proof key
 loolwsd-generate-proof-key
